5b641e348894303c72b43023a64496181ca2c679
Pranay Prakash <pranay.gp@gmail.com>
2018-03-01 02:32:30 -0600

    authenticate

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const { promisify } = require('util')
 const mongoose = require('mongoose')
 const AWS = require('aws-sdk')
 const uuid = require('node-uuid')
+const authenticate = require('mm-authenticate')(mongoose)
 const { Team, Script, Log } = require('mm-schemas')(mongoose)
 const { send, buffer } = require('micro')
 
@@ -15,18 +16,22 @@ const s3 = new AWS.S3({
 
 const getObject = promisify(s3.getObject.bind(s3))
 
-module.exports = async (req, res) => {
+module.exports = authenticate(async (req, res) => {
   const urlParams = req.url.split('/')
   if(urlParams.length !== 3) {
-    send(res, 400, 'Malformed URL')
-    return
+    return send(res, 400, 'Malformed URL')
   }
   const [_, ...teamNames] = urlParams
-  const logFile = await Log.findOne({index: teamNames.sort().join('-')}).exec()
+
+  // Get latest logfile
+  const index = teamNames.sort().join('-')
+  const logFile = await Log.findOne({index}).sort({createdAt: -1}).exec()
 
   if (!logFile) {
-    send(res, 404, `Teams ${teamNames.join(' and ')} don't have a log file`)
-    return;
+    return send(res, 404, `Teams ${teamNames.join(' and ')} don't have a log file`)
+  }
+  if(!logFile.canBeAccessedBy(req.user)){
+    return send(res, 401, 'Unauthorized')
   }
 
   const data = await getObject({
@@ -35,4 +40,4 @@ module.exports = async (req, res) => {
   })
 
   send(res, 200, data.Body)
-}
+})
